8c5882d9e23c490e7919115b0f223c7a2fcec193
Prasad-0317 <121848841+Prasad-0317@users.noreply.github.com>
2024-02-18 08:36:44 +0530

    Update Test.java

--- OOPS/exception_overloading/Test.java
+++ OOPS/exception_overloading/Test.java
@@ -1,4 +1,27 @@
 package OOPS.exception_overloading;
 
 public class Test {
+    void show(){
+        System.out.println("1");
+    }
 }
+
+class Xyz extends Test{
+    void show() throws RuntimeException{
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.show();
+        Xyz ob = new Xyz();
+        ob.show();
+    }
+}
+
+// if parent class has no exception then child can have unchecked exception
+//here arithmetic , runtime exc is unchecked exception
+// if exception is checked exception it gives compile error when parent has no exception
+
+// if parent has exception then child can have same level of exc or its child class exc or no exc
+// arithmetic and run are same level of exc
